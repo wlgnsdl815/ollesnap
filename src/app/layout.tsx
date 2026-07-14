66b427ddb054f67077696672e7fe04afbc63a899
wlgnsdl815 <wlgnsdl815@gmail.com>
2026-07-14 16:19:49 +0900

    design: logo 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "올레스냅",
   description: "제주 웨딩 스냅부터 여행 코스까지 설계하는 통합 플랫폼",
+  icons: {
+    icon: "/logo_camera.svg",
+  },
 };
 
 export default function RootLayout({
